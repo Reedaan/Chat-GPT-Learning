375fa0479895c3bb92a518847b26ec1b66906ef9
Reedaan <119610449+Reedaan@users.noreply.github.com>
2023-04-05 13:45:15 +0200

    Delete Chat GPT learning directory

--- Chat GPT learning/1_1_calculate_area/main.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Give side A: "))
-b = int(input("Give side B: "))
-
-result = a * b
-
-print(f"The area is: {result} cm^2")
--- Chat GPT learning/1_3_reverse_string/main.py
+++
@@ -1,5 +0,0 @@
-input = input("Provide a string to reverse it: ")
-
-reversed_string = input[::-1]
-
-print(reversed_string)
